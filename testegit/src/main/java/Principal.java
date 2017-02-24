4a50211470cf961dea8f2f1189ad38232ef957b7
glebson.lsilva1 <glebson.lsilva1@CAS555489W0731.senacsp.edu.br>
2017-02-24 20:56:01 -0300

    Correção na apresentação da mensagem

--- testegit/src/main/java/Principal.java
+++ testegit/src/main/java/Principal.java
@@ -7,10 +7,10 @@ public class Principal {
    
     public static void main(String[] args) {
         
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 50; i++){
         
             
-            System.out.println(i+" Hello world Senac PI3A!!!");
+            System.out.println(i+"**** Hello world Senac PI3A!!! ****");
         
         }
     }
