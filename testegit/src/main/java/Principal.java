af9a36ea9509e392af7660b21e95cde77c878fdc
glebson.lsilva1 <glebson.lsilva1@CAS555489W0731.senacsp.edu.br>
2017-02-24 20:35:17 -0300

    Versão inicial

---
+++ testegit/src/main/java/Principal.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author glebson.lsilva1
+ */
+public class Principal {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Hello world Senac PI3A!!!");
+    }
+    
+}
