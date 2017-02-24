0c644cd820917402756d6ce8e988d69c0c3cfed8
glebson.lsilva1 <glebson.lsilva1@CAS555489W0731.senacsp.edu.br>
2017-02-24 20:52:28 -0300

    Criação da Função para Impimir 10x

--- testegit/src/main/java/Principal.java
+++ testegit/src/main/java/Principal.java
@@ -4,13 +4,20 @@
  */
 public class Principal {
 
+    private static void mostrarMensagem(int i){
+    
+    
+        System.out.println(i+" Hello world Senac PI3A!!!");
+    
+    }
    
     public static void main(String[] args) {
         
         for(int i = 0; i < 10; i++){
         
             
-            System.out.println(i+" Hello world Senac PI3A!!!");
+            mostrarMensagem(i);
+            
         
         }
     }
